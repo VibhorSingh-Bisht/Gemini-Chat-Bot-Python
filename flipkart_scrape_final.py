0b97209cc4ca08ff5ffb9ae8ab961fca914f02f3
Vibhor <vibhor0711@gmail.com>
2024-06-15 12:24:39 +0530

    first commit

--- flipkart_scrape_final.py
+++ flipkart_scrape_final.py
@@ -26,13 +26,17 @@ def get_title(soup):
 def get_price(soup):
 
     try:
-        price = soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
+        price = soup.find("span", attrs={'class':'a-price-whole'})
+        price = price.text
+        price = '₹' + price
 
     except AttributeError:
 
         try:
             # If there is some deal price
-            price = soup.find("span", attrs={'id':'priceblock_dealprice'}).string.strip()
+            price = soup.find("span", attrs={'class':'a-price-whole'})
+            price = price.text
+            price = '₹' + price
 
         except:
             price = ""
@@ -77,55 +81,57 @@ def get_availability(soup):
 def main():
 
     # add your user agent
-    HEADERS = ({'User-Agent':'', 'Accept-Language': 'en-US, en;q=0.5'})
+    HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
 
     # The webpage URL
     TAGS = ['Mobile Phones',"Laptops","Television","Shoes"]
-    URLS = [f"https://www.flipkart.com/s?k={i.replace(' ','+')}&ref=nb_sb_noss" for i in TAGS]
+    URLS = [f"https://www.amazon.in/s?k={i.replace(' ','+')}&ref=nb_sb_noss" for i in TAGS]
 
-    amazon_df = None
-    for index,URL in enumerate(URLS):
-      # HTTP Request
-      webpage = requests.get(URL, headers=HEADERS)
+    #store links
+    links_list = []
+    for URL in URLS:
+        # HTTP Request
+        webpage = requests.get(URL, headers=HEADERS)
 
-      # Soup Object containing all data
-      soup = BeautifulSoup(webpage.content, "html.parser")
+        # Soup Object containing all data
+        soup = BeautifulSoup(webpage.content, "html.parser")
 
-      # Fetch links as List of Tag Objects
-      links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})
+        # Fetch links as List of Tag Objects
+        links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})
 
-      # Store the links
-      links_list = []
+        # Loop for extracting links from Tag Objects
+        for link in links:
+            link = link.get('href')
+            link = link.split('ref')[0]
+            links_list.append(link)
 
-      # Loop for extracting links from Tag Objects
-      for link in links:
-              links_list.append(link.get('href'))
+    d = {"title":[], "price":[], "rating":[], "reviews":[],"availability":[]}
 
-      d = {"title":[], "price":[], "rating":[], "reviews":[],"availability":[]}
+    # Loop for extracting product details from each link
+    for link in links_list:
+        try:
+            new_webpage = requests.get("https://www.amazon.in" + link, headers=HEADERS)
 
-      # Loop for extracting product details from each link
-      for link in links_list:
-          new_webpage = requests.get("https://www.flipkart.com" + link, headers=HEADERS)
+            new_soup = BeautifulSoup(new_webpage.content, "html.parser")
 
-          new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+            # Function calls to display all necessary product information
+            d['title'].append(get_title(new_soup))
+            d['price'].append(get_price(new_soup))
+            d['rating'].append(get_rating(new_soup))
+            d['reviews'].append(get_review_count(new_soup))
+            d['availability'].append(get_availability(new_soup))
+        except:
+            continue
 
-          # Function calls to display all necessary product information
-          d['title'].append(get_title(new_soup))
-          d['price'].append(get_price(new_soup))
-          d['rating'].append(get_rating(new_soup))
-          d['reviews'].append(get_review_count(new_soup))
-          d['availability'].append(get_availability(new_soup))
-
-
-      amazon_df = pd.DataFrame.from_dict(d)
-      amazon_df['title'].replace('', np.nan, inplace=True)
-      amazon_df = amazon_df.dropna(subset=['title'])
-      file_path = f'{os.getcwd()}\\data\\amazon_data.csv'
-      if not os.path.isfile(file_path):
+    amazon_df = pd.DataFrame.from_dict(d)
+    amazon_df = amazon_df.replace({'title': ''}, pd.NA)
+    amazon_df = amazon_df.dropna(subset=['title'])
+    file_path = f'{os.getcwd()}\\data\\amazon_data.csv'
+    if not os.path.isfile(file_path):
         amazon_df.to_csv(file_path, mode='w', index=False, header=True)
-      else:
+    else:
     
-        amazon_df.to_csv(file_path, mode='a', index=False, header=False)
+        amazon_df.to_csv(file_path, mode='a', index=False, header=False) 
 
 
 if __name__ == '__main__':
